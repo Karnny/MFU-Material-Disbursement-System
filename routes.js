ed53b0971ba66c8af3990e428b367412a6983f67
Orawan  Khongthip <72434691+OrawanKhongthip@users.noreply.github.com>
2021-04-23 05:59:02 +0700

    finish function of 3 page except fillter function

--- routes.js
+++ routes.js
@@ -7,14 +7,16 @@ function router(app) {
     res.sendFile(path.join(__dirname, "/views/index.html"));
   });
 
-  
+
   //other routes here..
   app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/login.html"));
   });
 
-  
-
+  //other routes here..
+  app.get("/test", (req, res) => {
+    res.render(path.join(__dirname, "/views/super admin/manageUsers.ejs"));
+  });
 
 }
 
